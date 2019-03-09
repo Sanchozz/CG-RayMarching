736818fe948c79ce0a383f45b1cc5ab7cc22f419
Sanchozz <knopochnik@mail.ru>
2019-03-09 23:23:36 +0300

    ShaderProgram fixed

--- ShaderProgram.h
+++ ShaderProgram.h
@@ -5,42 +5,50 @@
 #include "common.h"
 
 #include "LiteMath.h"
+using namespace LiteMath;
 
-class ShaderProgram {
+class ShaderProgram
+{
 public:
 
-    ShaderProgram() : shaderProgram(-1) {}
+  ShaderProgram() : shaderProgram(-1) {};
 
-    ShaderProgram(const std::unordered_map<GLenum, std::string> &inputShaders);
+  ShaderProgram(const std::unordered_map<GLenum, std::string> &inputShaders);
 
-    virtual ~ShaderProgram() {}
+  virtual ~ShaderProgram() {};
 
-    void Release(); //actual destructor
+  void Release(); //actual destructor
 
-    virtual void StartUseShader() const;
+  virtual void StartUseShader() const;
 
-    virtual void StopUseShader() const;
+  virtual void StopUseShader() const;
 
-    GLuint GetProgram() const { return shaderProgram; }
+  GLuint GetProgram() const { return shaderProgram; }
 
 
-    bool reLink();
+  bool reLink();
 
-    void SetUniform(const std::string &location, float value) const;
+  void SetUniform(const std::string &location, const float4x4 &value) const;
 
-    void SetUniform(const std::string &location, double value) const;
+  void SetUniform(const std::string &location, float value) const;
 
-    void SetUniform(const std::string &location, int value) const;
+  void SetUniform(const std::string &location, double value) const;
 
-    void SetUniform(const std::string &location, unsigned int value) const;
+  void SetUniform(const std::string &location, int value) const;
 
-    void SetUniform(const std::string &location, LiteMath::float4x4) const;
+  void SetUniform(const std::string &location, unsigned int value) const;
+
+  void SetUniform(const std::string &location, const float4 &value) const;
+
+  void SetUniform(const std::string &location, const float3 &value) const;
+
+  void SetUniform(const std::string &location, const float2 &value) const;
 
 private:
-    static GLuint LoadShaderObject(GLenum type, const std::string &filename);
+  static GLuint LoadShaderObject(GLenum type, const std::string &filename);
 
-    GLuint shaderProgram;
-    std::unordered_map<GLenum, GLuint> shaderObjects;
+  GLuint shaderProgram;
+  std::unordered_map<GLenum, GLuint> shaderObjects;
 };
 
 
